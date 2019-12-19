27347f01359f424e7870d3eff656ab499dc5e053
wabisque <wesleywebb35@gmail.com>
2019-12-19 04:37:05 +0000

    completion of the video splash screen scene

---
+++ Assets/Scripts/SplashScreen/VideoSplashScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoSplashScreen : MonoBehaviour
+{
+    // VideoPlayer component attached to this gameObject
+    VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        videoPlayer.loopPointReached += LoadActualGame;
+    }
+
+    // LoadActualGame loads the actual game
+    void LoadActualGame(VideoPlayer vPlayer)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
