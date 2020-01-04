5a520f27fa4be0abca107b4c49c396654402ad03
wabisque <wesleywebb35@gmail.com>
2020-01-04 16:34:15 +0000

    Main Menu Separation

---
+++ Assets/Wesley's changes/Scripts/MainMenuManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject[] menuScreens;
+    public int initialScreenIndex = 0;
+
+    void OnEnable()
+    {
+        ShowOnlyScreen(initialScreenIndex);
+    }
+
+    public void ShowOnlyScreen(int index)
+    {
+        if (index >= menuScreens.Length) return;
+
+        Array.ForEach(menuScreens, screen => screen.SetActive(false));
+        menuScreens[index].SetActive(true);
+    }
+
+    public void ShowOnlyScreen(string name)
+    {
+        GameObject targetScreen = Array.Find(menuScreens, screen => screen.name == name);
+
+        if (!targetScreen) return;
+
+        Array.ForEach(menuScreens, screen => screen.SetActive(false));
+        targetScreen.SetActive(true);
+    }
+
+    public void LoadLesson(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex, LoadSceneMode.Single);
+    }
+
+    public void LoadLesson(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
