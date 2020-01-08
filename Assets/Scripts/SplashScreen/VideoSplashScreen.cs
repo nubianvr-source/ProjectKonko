f304e7fd8501dfcb77566b4b1b57ab28f31fdfdf
wabisque <wesleywebb35@gmail.com>
2020-01-08 12:29:16 +0000

    Video SplashScreen System now supports multiple videos

--- Assets/Scripts/SplashScreen/VideoSplashScreen.cs
+++ Assets/Scripts/SplashScreen/VideoSplashScreen.cs
@@ -6,24 +6,58 @@ using UnityEngine.Video;
 
 public class VideoSplashScreen : MonoBehaviour
 {
+    public VideoClip[] splashClips;
+
     // VideoPlayer component attached to this gameObject
     VideoPlayer videoPlayer;
+
+    int currentClip = 0;
 
     // Start is called before the first frame update
     void Awake()
     {
         videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.clip = splashClips[currentClip];
+        videoPlayer.Play();
     }
 
     // Start is called before the first frame update
     void OnEnable()
     {
-        videoPlayer.loopPointReached += LoadActualGame;
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+        {
+            NextClip();
+        }
+    }
+
+
+    public void NextClip()
+    {
+        currentClip++;
+
+        if (currentClip >= splashClips.Length)
+        {
+            LoadActualGame();
+            return;
+        }
+
+        videoPlayer.clip = splashClips[currentClip];
+        videoPlayer.Play();
     }
 
     // LoadActualGame loads the actual game
-    void LoadActualGame(VideoPlayer vPlayer)
+    void LoadActualGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    void OnVideoEnd(VideoPlayer vPlayer)
+    {
+        NextClip();
     }
 }
